90b1b322a001b6f6933f56a499ee3283e9fcab13
Harish Palani <harishpalani3@gmail.com>
2017-10-25 23:42:53 +0000

    Content complete!

--- main.cpp
+++ main.cpp
@@ -18,51 +18,81 @@
 
 using namespace std;
 
-int makeEqualLength(string &str1, string &str2)
-{
-    int len1 = str1.size();
-    int len2 = str2.size();
-    if (len1 < len2)
-    {
-        for (int i = 0 ; i < len2 - len1 ; i++)
-            str1 = '0' + str1;
-        return len2;
+string result;
+int sum = 0, carry = 0;
+
+int equalize(string &str1, string &str2);
+bool isBinary(string str);
+string reformatBinaryString(const string &str);
+string removeSpaces(string str);
+void HALFADDER(int firstBit, int secondBit);
+string FULLADDER(string first, string second);
+
+int main() {
+    for (int i = 0; i < 4; i++) {
+        cout << "\nROUND "
+             << i + 1
+             << endl;
+        
+        string p, q;
+        
+        cout << "\nBinary Term #1"
+             << endl;
+        getline(cin, p);
+        p = removeSpaces(p);
+        
+        while (!isBinary(p)) {
+            cout << "\nPlease enter a valid binary term."
+                 << endl;
+            getline(cin, p);
+            p = removeSpaces(p);
+        }
+        
+        cout << "\nBinary Term #2"
+             << endl;
+        getline(cin, q);
+        q = removeSpaces(q);
+        
+        while (!isBinary(q)) {
+            cout << "\nPlease enter a valid binary term."
+                 << endl;
+            getline(cin, q);
+            q = removeSpaces(q);
+        }
+        
+        cout << "\nThe answer is "
+             << reformatBinaryString(FULLADDER(p, q))
+             << "."
+             << endl;
+        
+        sum = 0;
+        carry = 0;
+        result.clear();
     }
-    else if (len1 > len2)
-    {
-        for (int i = 0 ; i < len1 - len2 ; i++)
-            str2 = '0' + str2;
-    }
-    return len1;
 }
 
-string reformatBinaryString(const string &s) {
-   if (!s.size()) { return ""; }
-   
-   std::stringstream ss;
-   ss << s[0];
-   
-   for (int i = 1; i < s.size(); i++) {
-       if ((i % 4) == 0) {
-        ss << ' ' << s[i];
-       } else {
-           ss << s[i];
-       }
-   }
-   
-   return ss.str();
-}
-
-string removeSpaces(string str) {
-    str.erase(remove(str.begin(), str.end(), ' '), str.end());
-    return str;
-}
-
-string ADDER(string first, string second) {
+void HALFADDER(int firstBit, int secondBit) {
+    cout << "The carry in is " 
+         << carry
+         << ".     ";
     
-    string result;
-    int length = makeEqualLength(first, second);
-    int carry = 0;
+    sum = (firstBit ^ secondBit ^ carry)+'0';
+    
+    cout << "The sum is " 
+         << sum-'0'
+         << ".     ";
+     
+    result = (char)sum + result;
+    carry = (firstBit & secondBit) | (secondBit & carry) | (firstBit & carry);
+    
+    cout << "The carry out is " 
+         << carry
+         << ".     "
+         << endl;
+}
+
+string FULLADDER(string first, string second) {
+    int length = equalize(first, second);
     
     cout << "\nThe numbers to be added are " 
          << reformatBinaryString(first)
@@ -77,28 +107,12 @@ string ADDER(string first, string second) {
         int secondBit = second.at(i) - '0';
         
         cout << "The bits are " 
-         << firstBit
-         << " and "
-         << secondBit
-         << ".     ";
+             << firstBit
+             << " and "
+             << secondBit
+             << ".     ";
         
-        cout << "The carry in is " 
-         << carry
-         << ".     ";
-        
-        int sum = (firstBit ^ secondBit ^ carry)+'0';
-        
-        cout << "The sum is " 
-         << sum-'0'
-         << ".     ";
-         
-        result = (char)sum + result;
-        carry = (firstBit & secondBit) | (secondBit & carry) | (firstBit & carry);
-        
-        cout << "The carry out is " 
-         << carry
-         << ".     "
-         << endl;
+        HALFADDER(firstBit, secondBit);
     }
     
     if (carry) {
@@ -108,21 +122,54 @@ string ADDER(string first, string second) {
     return result;
 }
 
-int main() {
-    string p, q;
+int equalize(string &str1, string &str2) {
+    int len1 = str1.size();
+    int len2 = str2.size();
     
-    cout << "\nBinary Term #1 "
-         << endl;
-    getline(cin, p);
-    p = removeSpaces(p);
+    if (len1 < len2) {
+        for (int i = 0 ; i < len2 - len1 ; i++) {
+            str1 = '0' + str1;
+        }
+        return len2;
+    } else if (len1 > len2) {
+        for (int i = 0 ; i < len1 - len2 ; i++) {
+            str2 = '0' + str2;
+        }
+    }
     
-    cout << "\nBinary Term #2 "
-         << endl;
-    getline(cin, q);
-    q = removeSpaces(q);
-    
-    cout << "\nThe answer is "
-         << reformatBinaryString(ADDER(p, q))
-         << "."
-         << endl;
+    return len1;
+}
+
+bool isBinary(string str) {
+    bool isBinary = true;
+    for (int i = 0; i < str.length(); i++) {
+        if (str.at(i) != '0' && str.at(i) != '1') {
+            isBinary = false;
+        }
+   }
+   return isBinary;
+}
+
+string removeSpaces(string str) {
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    return str;
+}
+
+string reformatBinaryString(const string &str) {
+   if (!str.size()) {
+       return "";
+   }
+   
+   std::stringstream ss;
+   ss << str[0];
+   
+   for (int i = 1; i < str.size(); i++) {
+       if ((i % 4) == 0) {
+        ss << ' ' << str[i];
+       } else {
+           ss << str[i];
+       }
+   }
+   
+   return ss.str();
 }
